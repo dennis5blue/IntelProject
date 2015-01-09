2a23911aef4732b0f1172916c64e1c3a6336c4cd
dennis5blue <dennis5blue@gmail.com>
2015-01-09 13:12:55 +0800

    update

--- SimpleDemo/FileClient/src/Client_gui/Check_file.java
+++ SimpleDemo/FileClient/src/Client_gui/Check_file.java
@@ -4,29 +4,53 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+//import java.util.Set;
+//import java.util.HashSet;
+import java.util.ArrayList;
 
 //import org.omg.CORBA.INTERNAL;
 
 public class Check_file {
 	public Check_file (){
-		line = null;
+		line1 = null;
+		line2 = null;
 		
 	}
-	public int Check (final String filename)  {
-		try{  
-	    FileReader fr = new FileReader(filename);
-	    BufferedReader br = new BufferedReader(fr);
-	    while ((line = br.readLine())!=null){
-	    	return Integer.valueOf(line);
+	public int Check (final String filename1, final String filename2)  { // file 1 has size 1, file 2 has size 2 
+		try{
+	    FileReader fr1 = new FileReader(filename1);
+	    FileReader fr2 = new FileReader(filename2);
+	    BufferedReader br1 = new BufferedReader(fr1);
+	    BufferedReader br2 = new BufferedReader(fr2);
+	    while ((line1 = br1.readLine())!=null && (line2 = br2.readLine())!=null){
+	    	String[] tokens1 = line1.split(" ");
+	    	String[] tokens2 = line2.split(" ");
+	    	ArrayList<Integer> array1 = new ArrayList<Integer>();
+	    	array1.add(Integer.valueOf(tokens1[0]));
+	    	ArrayList<Integer> array2 = new ArrayList<Integer>();
+	    	int i;
+	    	for (i=0; i<tokens2.length; i++){
+	    		array2.add(Integer.valueOf(tokens2[i]));
+	    	}
+	    	int id = 0;
+	    	for (i=0; i<array2.size(); i++){
+	    		if (array2.get(i) == array1.get(0))
+	    			id = array2.get(i);
+	    	}
+	    	//System.out.println(id);
+	    	fr1.close();
+		    fr2.close();
+		    br1.close();
+		    br2.close();
+	    	return id;
 	    }
-	    fr.close();
-	    br.close();
-		}catch (FileNotFoundException e) {
+	    }catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}catch (IOException e) {
 			e.printStackTrace();
 		}
         return 0 ;
 	  }
-	public static String line;
+	public static String line1;
+	public static String line2;
 }
--- SimpleDemo/FileClient/src/Client_gui/SimpleFileClient.java
+++ SimpleDemo/FileClient/src/Client_gui/SimpleFileClient.java
@@ -1,5 +1,6 @@
 package Client_gui;
 import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.Socket;
@@ -7,11 +8,15 @@ import java.net.Socket;
 public class SimpleFileClient {
 	public final static int SOCKET_PORT = 13267;
 	public final static String SERVER1 = "192.168.21.222";
-	public final static String SERVER2 = "192.168.21.224";
+	public final static String SERVER2 = "192.168.137.1";
 	public final static String
 	FILE_TO_RECEIVED = "/Users/dennisyu/Desktop/photo.jpg";  // you may change this, I give a
                                                             // different name because i don't want to
                                                             // overwrite the one used by server...
+	public final static String
+	INPUT_FILE_NAME1 = "/Users/dennisyu/Documents/LAB/IntelProject/SimpleDemo/FileClient/src/Client_gui/output1.txt";
+	public final static String
+	INPUT_FILE_NAME2 = "/Users/dennisyu/Documents/LAB/IntelProject/SimpleDemo/FileClient/src/Client_gui/output2.txt";
 
 	public final static int FILE_SIZE = 6022386; // file size temporary hard coded
                                                  // should bigger than the file to be downloaded
@@ -28,9 +33,13 @@ public class SimpleFileClient {
 		Check_file check_file = new Check_file();
 		try {
 			while(true){	
-				Thread.sleep(1000);
-				int car_id = check_file.Check("/Users/dennisyu/Documents/LAB/IntelProject/SimpleDemo/FileClient/src/Client_gui/ComportOutput.txt");
-      
+				//Thread.sleep(1000);
+				File file1 = new File(INPUT_FILE_NAME1);
+				File file2 = new File(INPUT_FILE_NAME2);
+				int car_id = arr;
+				if (file1.exists() && file2.exists()){
+					car_id = check_file.Check(INPUT_FILE_NAME1,INPUT_FILE_NAME2);	
+				}
 				if (arr != car_id){
 					System.out.println(car_id);
 					if (car_id == 1 ){
@@ -57,8 +66,9 @@ public class SimpleFileClient {
 					bos.flush();
 					System.out.println("File " + FILE_TO_RECEIVED + " downloaded (" + current + " bytes read)");
 					Imagecode.exit();
-					Imagecode.Image("/Users/dennisyu/Desktop/photo.jpg");
+					Imagecode.Image(FILE_TO_RECEIVED);
 					arr = car_id;
+					sock.close();
 				}
       
 			}
